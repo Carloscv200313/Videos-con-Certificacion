5e21eafd9159c16c86c898ad8d73eb639b5bc68c
Carloscv200313 <carlosscv200313@gmail.com>
2025-01-14 19:21:25 -0500

    manejo de errores solucionado

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -7,8 +7,8 @@ export const Error = () => {
 
             <div className="flex flex-col items-center justify-center">
                 <p className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-wider text-gray-600 mt-8">500</p>
-                <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-600 mt-2">Server Error</p>
-                <p className="md:text-lg xl:text-xl text-gray-500 mt-4">Whoops, something went wrong on our servers.</p>
+                <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-600 mt-2">Error del servidor</p>
+                <p className="md:text-lg xl:text-xl text-gray-500 mt-4">Vaya, algo salió mal en nuestros servidores...</p>
             </div>
         </div>
     )
